1998f70a8093c12ed366368764495bebd6b42388
Matthew Schad <mascha@microsoft.com>
2021-03-20 12:53:31 -0400

    instantiate window and show it after ready-to-show event is emitted

--- app/main.js
+++ app/main.js
@@ -1,1 +1,16 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+	console.log("It's ready");
+	mainWindow = new BrowserWindow({
+		show: false,
+	});
+	mainWindow.loadFile(`${__dirname}/index.html`);
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+	});
+});
+
+console.log('Starting Application');
