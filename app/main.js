a5157ffa0c8f50c771804c57d0a61c97152a1328
Matthew Schad <mascha@microsoft.com>
2021-03-21 12:09:14 -0400

    learn fundamental main/renderer process concepts

--- app/main.js
+++ app/main.js
@@ -1,16 +1,139 @@
-const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const { app, BrowserWindow, dialog, Menu } = require('electron');
 
 let mainWindow = null;
 
 app.on('ready', () => {
-	console.log("It's ready");
 	mainWindow = new BrowserWindow({
 		show: false,
 	});
+	Menu.setApplicationMenu(applicationMenu);
 	mainWindow.loadFile(`${__dirname}/index.html`);
 	mainWindow.once('ready-to-show', () => {
 		mainWindow.show();
 	});
 });
 
-console.log('Starting Application');
+exports.getFileFromUser = () => {
+	const files = dialog.showOpenDialog(mainWindow, {
+		properties: ['openFile'],
+		buttonLabel: 'Choose this file',
+		title: 'Open Text Document',
+		filters: [
+			{ name: 'Markdown Files', extensions: ['md', 'mdown', 'markdown'] },
+			{ name: 'Text Files', extensions: ['txt', 'text'] },
+		],
+	});
+
+	if (!files) {
+		return;
+	}
+	const file = files[0];
+	openFile(file);
+};
+
+exports.saveMarkdown = (file, content) => {
+	if (!file) {
+		file = dialog.showSaveDialog(mainWindow, {
+			title: 'Save Markdown File',
+			defaultPath: app.getPath('desktop'),
+			buttonLabel: 'Save Markdown File',
+			filters: [
+				{
+					name: 'Markdown Files',
+					extensions: ['md', 'markdown', 'mdown'],
+				},
+			],
+		});
+	}
+
+	if (!file) {
+		return;
+	}
+
+	fs.writeFileSync(file, content);
+	openFile(file);
+};
+
+exports.saveHtml = (content) => {
+	const file = dialog.showSaveDialog(mainWindow, {
+		title: 'Save HTML File',
+		defaultPath: app.getPath('desktop'),
+		buttonLabel: 'Save HTML File',
+		filters: [
+			{
+				name: 'HTML Files',
+				extensions: ['html'],
+			},
+		],
+	});
+
+	if (!file) {
+		return;
+	}
+
+	fs.writeFileSync(file, content);
+};
+
+const openFile = (exports.openFile = (file) => {
+	const content = fs
+		.readFileSync(file, { encoding: 'utf8' })
+		.toString()
+		// strip carriage returns because HTMLInputElement.value strips these
+		.replace(/\r/gm, '');
+
+	app.addRecentDocument(file);
+	mainWindow.webContents.send('file-opened', file, content);
+});
+
+const menuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Open File',
+				accelerator: 'CommandOrControl+O',
+				click() {
+					exports.getFileFromUser();
+				},
+			},
+			{
+				label: 'Save File',
+				accelerator: 'CommandOrControl+S',
+				click() {
+					mainWindow.webContents.send('save-markdown');
+				},
+			},
+			{
+				label: 'Save HTML',
+				accelerator: 'CommandOrControl+Shift+S',
+				click() {
+					mainWindow.webContents.send('save-html');
+				},
+			},
+			{
+				label: 'Copy',
+				role: 'copy',
+			},
+		],
+	},
+];
+
+if (process.platform === 'darwin') {
+	const applicationName = 'Fire Sale';
+	menuTemplate.unshift({
+		label: applicationName,
+		submenu: [
+			{
+				label: `About ${applicationName}`,
+				role: 'about',
+			},
+			{
+				label: `Quit ${applicationName}`,
+				role: 'quit',
+			},
+		],
+	});
+}
+
+const applicationMenu = Menu.buildFromTemplate(menuTemplate);
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,12 @@
+const path = require('path');
 const marked = require('marked');
+const { remote, ipcRenderer, shell } = require('electron');
+
+let filePath = null;
+let originalContent = '';
+
+const mainProcess = remote.require(`${__dirname}/main.js`);
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -10,11 +18,115 @@ const saveHtmlButton = document.querySelector('#save-html');
 const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
-const renderMarkdownToHtml = markdown => {
-  htmlView.innerHTML = marked(markdown, { sanitize: true });
+const renderMarkdownToHtml = (markdown) => {
+	htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
-markdownView.addEventListener('keyup', event => {
-  const currentContent = event.target.value;
-  renderMarkdownToHtml(currentContent);
+const updateUserInterface = (isEdited) => {
+	let title = 'Fire Sale';
+	if (filePath) {
+		title = `${path.basename(filePath)} - ${title}`;
+	}
+	if (isEdited) {
+		title = `${title} (Edited)`;
+	}
+	if (filePath) {
+		currentWindow.setRepresentedFilename(filePath);
+	}
+
+	showFileButton.disabled = !filePath;
+	openInDefaultButton.disabled = !filePath;
+
+	currentWindow.setDocumentEdited(isEdited);
+	saveMarkdownButton.disabled = !isEdited;
+	revertButton.disabled = !isEdited;
+	currentWindow.setTitle(title);
+};
+
+markdownView.addEventListener('keyup', (event) => {
+	const currentContent = event.target.value;
+
+	renderMarkdownToHtml(currentContent);
+	updateUserInterface(currentContent !== originalContent);
+});
+
+openFileButton.addEventListener('click', () => {
+	mainProcess.getFileFromUser();
+});
+
+const saveMarkdown = () => {
+	mainProcess.saveMarkdown(filePath, markdownView.value);
+};
+
+const saveHTML = () => {
+	mainProcess.saveHtml(htmlView.innerHTML);
+};
+
+saveMarkdownButton.addEventListener('click', saveMarkdown);
+saveHtmlButton.addEventListener('click', saveHTML);
+
+ipcRenderer.on('save-markdown', saveMarkdown);
+ipcRenderer.on('save-html', saveHTML);
+
+showFileButton.addEventListener('click', () => {
+	if (!filePath) {
+		return alert('No file path');
+	}
+
+	shell.showItemInFolder(filePath);
+});
+
+openInDefaultButton.addEventListener('click', () => {
+	if (!filePath) {
+		return alert('There is no file to be opened.');
+	}
+	shell.openItem(filePath);
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+	filePath = file;
+	originalContent = content;
+	markdownView.value = content;
+
+	renderMarkdownToHtml(content);
+	updateUserInterface(false);
+});
+
+document.addEventListener('dragstart', (event) => event.preventDefault());
+document.addEventListener('dragover', (event) => event.preventDefault());
+document.addEventListener('dragleave', (event) => event.preventDefault());
+document.addEventListener('drop', (event) => event.preventDefault());
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+const isFileTypeSupported = (file) => {
+	// add empty type since markdown files don't return text/markdown in v8/blink...
+	return ['text/plain', 'text/markdown', ''].includes(file.type);
+};
+
+markdownView.addEventListener('dragover', (event) => {
+	const file = getDraggedFile(event);
+
+	if (isFileTypeSupported(file)) {
+		markdownView.classList.add('drag-over');
+	} else {
+		markdownView.classList.add('drag-error');
+	}
+});
+
+markdownView.addEventListener('dragleave', () => {
+	markdownView.classList.remove('drag-over');
+	markdownView.classList.remove('drag-error');
+});
+
+markdownView.addEventListener('drop', (event) => {
+	const file = getDroppedFile(event);
+
+	if (isFileTypeSupported(file)) {
+		mainProcess.openFile(file.path);
+	} else {
+		alert(
+			'That file type is not supported, only .txt or .md files are accepted'
+		);
+	}
 });
